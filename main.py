ea05f542affcdb05a024e0bf98f952287f120ceb
Santiago Lorenzano <enderwigim@gmail.com>
2022-10-19 13:01:55 +0200

    Trying to solve Heroku problems

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ ckeditor = CKEditor(app)
 Bootstrap4(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
